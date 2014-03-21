291af0f99255244e8d387889b7a1c99233c517bf
Timur Kalimov <tkalimov@gmail.com>
2014-03-21 17:34:18 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.2'
+gem 'pg'
+gem 'unicorn'
+gem 'rails_12factor'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DecisionMaker::Application.config.secret_key_base = '7c3c38135548d7b5e13ae677c58ccecf1e2e1fd010fd00faba9463a28d36a157583f9aebdb14f2395e4e9a51398193496d0712e09a8815b27a688da2b5f6c4c1'
